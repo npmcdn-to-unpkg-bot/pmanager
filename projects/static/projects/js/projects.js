4b335bcbdf676e30d1e737693734abfc91ccf2b1
David Varner <david.varner@nih.gov>
2016-08-25 13:57:34 -0400

    Add jquery and get React pulling data from REST API

--- projects/static/projects/js/projects.js
+++ projects/static/projects/js/projects.js
@@ -1,13 +1,66 @@
-var ProjectBox = React.createClass({
+var data = [
+  {id: 1, name: "NOVA Open", desc: "Paint queue for NOVA"},
+  {id: 2, name: "30k Escalation", desc: "Paint queue for 30k Raven Guard"}
+];
+
+var Project = React.createClass({
   render: function() {
     return (
-      <div className="projectBox">
-        Hello, world! I am a ProjectBox.
+      <div className="project">
+        <h2 className="projectName">
+          {this.props.name}
+        </h2>
+        {this.props.children}
       </div>
     );
   }
 });
+
+var ProjectList = React.createClass({
+  render: function() {
+    var projectNodes = this.props.data.map(function(project) {
+      return (
+        <Project name={project.title} key={project.id}>
+          {project.description}
+        </Project>
+      );
+    });
+    return (
+      <div className="projectList">
+        {projectNodes}
+      </div>
+    );
+  }
+});
+
+var ProjectBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return (
+      <div className="projectBox">
+        <h1>Projects</h1>
+        <ProjectList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <ProjectBox />,
+  <ProjectBox url="api/projects/" />,
   document.getElementById('reactContent')
 );
